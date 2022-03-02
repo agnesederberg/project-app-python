27777b8ad5c7f366d0a4acee5082abed38c8a4b3
Agnes Ederberg <ederberg@net.chalmers.se>
2022-03-02 14:09:12 +0100

    kör

--- website/models.py
+++ website/models.py
@@ -2,22 +2,36 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-# gör en klass för folder, data= string - göt datan till en knapp i html
-
+# These are our dataclasses that represent the tables in our database modeling. They are commented out because we  
+# are working to connecting our user, folder and note class in three steps. These steps are not finished yet but our classes are defined.
 
 #class Folder(db.Model):
-#   id=
-#   data= db.Column(db.String(100))
-#   user_id = foreinkey
-#   date= ska vi ha date här?
+#   id= db.Column(db.Integer, primary_key=True)
+#   data= db.Column(db.String(50))
+#   date= db.Column(db.DateTime(timezone=True), default=func.now())
+#   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 #   user = db.relationship('User')
-#   content = data?? se ovan 
-# ändra user_id till typ folder_id på class note, sammankopplar folder till id, sammankollar note till folder och då blir alla sammankopplat till user    
 
 #class Category(db.Model):
-#   id=
-#   data=
-#   folder_id= foreinkey?
+#   id= db.Column(db.Integer, primary_key=True)
+#   data= db.Column(db.String(50))
+#   folder_id= db.Column(db.Integer, db.ForeignKey('folder.id'))
+#   folder = db.relationship('Folder')
+
+#class Note(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+ #   data = db.Column(db.String(10000))
+  #  date = db.Column(db.DateTime(timezone=True), default=func.now())
+ #   folder_id = db.Column(db.Integer, db.ForeignKey('folder.id'))
+#    folder = db.relationship('Folder')
+
+
+#class User(db.Model, UserMixin):
+ #   id = db.Column(db.Integer, primary_key=True)
+  #  email = db.Column(db.String(150), unique=True)
+   # password = db.Column(db.String(150))
+   # first_name = db.Column(db.String(150))
+   # folder = db.relationship('Folder') 
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,3 +46,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note') #ändra till folder-relationship
+
+
+
+
+
+
+
+    # ändra user_id till typ folder_id på class note, sammankopplar folder till id, sammankollar note till folder och då blir alla sammankopplat till user    
+# gör en klass för folder, data= string - gör datan till en knapp i html
+
